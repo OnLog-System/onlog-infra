0466ae13e07af6af7c0a601101e3593254bcb5eb
yoonseok.Kang <kangcl1609@google.com>
2025-11-29 18:40:00 +0900

    add VPC variables for CIDR, subnets, availability zones, and NAT configuration in Terraform

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDRs for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of AZs to assign to subnets"
+  type        = list(string)
+}
+
+variable "enable_nat" {
+  description = "Whether to create a NAT gateway"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_az" {
+  description = "AZ where the NAT Gateway will be placed"
+  type        = string
+}
