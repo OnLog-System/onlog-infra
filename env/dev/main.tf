870185d158492fe717371c7a56eb84dcf6fdcbb5
yoonseok.Kang <kangcl1609@google.com>
2025-11-29 18:40:08 +0900

    replace null_resource with VPC module configuration in Terraform for dev and prod environments

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,5 +1,21 @@
-resource "null_resource" "dev_test" {
-  triggers = {
-    env = "dev"
-  }
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr             = "10.0.0.0/16"
+  availability_zones   = ["ap-northeast-2a", "ap-northeast-2b"]
+
+  public_subnet_cidrs  = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.11.0/24",
+    "10.0.12.0/24",
+    "10.0.21.0/24",
+    "10.0.22.0/24"
+  ]
+
+  enable_nat   = true
+  single_nat_az = "ap-northeast-2a"
 }
