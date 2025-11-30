e799da327e15fcf9affe8dc203cbbce80fbad1c2
yoonseok.Kang <kangcl1609@gmail.com>
2025-11-30 16:29:00 +0900

    feat: add Terraform variables for VPC configuration

---
+++ env/dev/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "enable_nat" {
+  type = bool
+}
+
+variable "single_nat_az" {
+  type = string
+}
