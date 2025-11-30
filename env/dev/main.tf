0dac4028788d8b41f6c1a694944f03394ad5ceae
yoonseok.Kang <kangcl1609@gmail.com>
2025-11-30 16:28:55 +0900

    feat: parameterize VPC module variables for better configurability

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,21 +1,10 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_cidr           = "10.0.0.0/16"
-  availability_zones = ["ap-northeast-2a", "ap-northeast-2b"]
-
-  public_subnet_cidrs = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
-
-  private_subnet_cidrs = [
-    "10.0.11.0/24",
-    "10.0.12.0/24",
-    "10.0.21.0/24",
-    "10.0.22.0/24"
-  ]
-
-  enable_nat    = true
-  single_nat_az = "ap-northeast-2a"
+  vpc_cidr             = var.vpc_cidr
+  availability_zones   = var.availability_zones
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  enable_nat           = var.enable_nat
+  single_nat_az        = var.single_nat_az
 }
