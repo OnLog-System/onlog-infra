8e35bf55fc63911a09aab9fc44929a9fbd8720a1
yoonseok.Kang <kangcl1609@google.com>
2025-12-01 17:11:48 +0900

    feat: add outputs and variables for VPC and security group configurations

---
+++ env/dev/outputs.tf
@@ -0,0 +1,42 @@
+############################################################
+# VPC & Subnet
+############################################################
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "Public Subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnets" {
+  description = "Private Subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+############################################################
+# Security Groups
+############################################################
+
+output "sg_node" {
+  description = "NodeGroup Security Group ID"
+  value       = module.sg_node.id
+}
+
+output "sg_alb" {
+  description = "ALB Security Group ID"
+  value       = module.sg_alb.id
+}
+
+output "sg_controlplane" {
+  description = "Control Plane Security Group ID"
+  value       = module.sg_controlplane.id
+}
+
+output "sg_endpoints" {
+  description = "VPC Endpoint Security Group ID"
+  value       = module.sg_endpoints.id
+}
--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -1,3 +1,27 @@
+############################################################
+# 환경 기본 정보
+############################################################
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+############################################################
+# VPC 설정
+############################################################
+
 variable "vpc_cidr" {
   type = string
 }
@@ -20,4 +44,41 @@ variable "enable_nat" {
 
 variable "single_nat_az" {
   type = string
+}
+
+############################################################
+# EKS ControlPlane 관련
+############################################################
+
+variable "admin_cidrs" {
+  description = "kubectl 접근 허용 CIDR 목록"
+  type        = list(string)
+}
+
+############################################################
+# VPC Endpoint 관련 설정
+############################################################
+
+variable "endpoint_subnet_group" {
+  description = "endpoint 를 private subnet 에 만들지 public 에 만들지"
+  type        = string
+}
+
+############################################################
+# NAT Instance 옵션 (나중 대비)
+############################################################
+
+variable "enable_nat_instance" {
+  type    = bool
+  default = false
+}
+
+variable "nat_instance_type" {
+  type    = string
+  default = "t4g.nano"
+}
+
+variable "nat_instance_az" {
+  type    = string
+  default = "ap-northeast-2a"
 }
