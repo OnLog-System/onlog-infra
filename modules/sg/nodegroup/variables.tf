60e3805620645f81bf7ab6269f38e4e278fc3b8a
yoonseok.Kang <kangcl1609@google.com>
2025-12-01 17:11:15 +0900

    feat: add AWS security group configuration for EKS NodeGroup with ingress and egress rules

---
+++ modules/sg/nodegroup/variables.tf
@@ -0,0 +1,22 @@
+variable "name" {}
+variable "vpc_id" {}
+
+variable "alb_sg_ids" {
+  type = list(string)
+}
+
+variable "controlplane_sg_ids" {
+  type = list(string)
+}
+
+variable "endpoint_sg_ids" {
+  type = list(string)
+}
+
+variable "app_port_min" { default = 30000 }
+variable "app_port_max" { default = 32767 }
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
