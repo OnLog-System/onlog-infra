99e277bd9cf5cacff2405a282b273ba83ca91897
yoonseok.Kang <kangcl1609@google.com>
2025-12-01 18:11:27 +0900

    feat: rename SG node group to 'node-base' for clarity

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -25,7 +25,7 @@ module "vpc" {
 module "sg_node_base" {
   source = "../../modules/sg/nodegroup"
 
-  name = "node"
+  name = "node-base"
   vpc_id = module.vpc.vpc_id
   environment = var.environment
 
