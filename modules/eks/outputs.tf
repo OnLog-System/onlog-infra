6853bc49fb9786c620be4a79008f44a2dcf6bb2b
yoonseok.Kang <kangcl1609@google.com>
2026-01-03 20:12:04 +0900

    feat: add outputs for OIDC provider and node role ARNs in EKS module

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -9,3 +9,11 @@ output "cluster_endpoint" {
 output "cluster_arn" {
   value = var.enable ? aws_eks_cluster.this[0].arn : null
 }
+
+output "oidc_provider_arn" {
+  value = var.enable ? aws_iam_openid_connect_provider.eks[0].arn : null
+}
+
+output "node_role_arn" {
+  value = var.enable ? aws_iam_role.eks_node[0].arn : null
+}
