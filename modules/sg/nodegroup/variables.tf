760af1ac3e97726897f9d39005764a1137e0d651
yoonseok.Kang <kangcl1609@google.com>
2026-01-03 21:40:34 +0900

    refactor: update NodeGroup section header and remove unused variable for EICE SG IDs

--- modules/sg/nodegroup/variables.tf
+++ modules/sg/nodegroup/variables.tf
@@ -8,9 +8,3 @@ variable "tags" {
   type    = map(string)
   default = {}
 }
-
-variable "eice_sg_ids" {
-  type        = list(string)
-  description = "Security Groups allowed to SSH into the nodes (EICE)"
-  default     = []
-}
