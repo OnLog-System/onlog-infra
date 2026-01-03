ee3a1e040c862d262f8732ff45c159d64aa951ff
yoonseok.Kang <kangcl1609@google.com>
2026-01-03 21:38:18 +0900

    refactor: remove unused ingress rules and related variables from node security group

--- modules/sg/nodegroup/main.tf
+++ modules/sg/nodegroup/main.tf
@@ -3,15 +3,6 @@ resource "aws_security_group" "node" {
   description = "EKS NodeGroup SG"
   vpc_id      = var.vpc_id
 
-  # Allow ALB → NodeGroup
-  ingress {
-    description     = "ALB to NodeGroup"
-    from_port       = var.app_port_min
-    to_port         = var.app_port_max
-    protocol        = "tcp"
-    security_groups = var.alb_sg_ids
-  }
-
   # Allow NodeGroup internal communication (Pod ↔ Pod, Node ↔ Node)
   ingress {
     description = "Node internal communication"
@@ -19,33 +10,6 @@ resource "aws_security_group" "node" {
     to_port     = 65535
     protocol    = "tcp"
     self        = true
-  }
-
-  # Allow Control Plane → NodeGroup
-  ingress {
-    description     = "ControlPlane to Node"
-    from_port       = 10250
-    to_port         = 10250
-    protocol        = "tcp"
-    security_groups = var.controlplane_sg_ids
-  }
-
-  # Allow EICE → Node (SSH)
-  ingress {
-    description     = "EICE to Node SSH"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = var.eice_sg_ids
-  }
-
-  # Outbound: Node → AWS Endpoints
-  egress {
-    description     = "Node to Endpoints"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = var.endpoint_sg_ids
   }
 
   # Outbound: internal node communication
--- modules/sg/nodegroup/variables.tf
+++ modules/sg/nodegroup/variables.tf
@@ -4,21 +4,6 @@ variable "environment" {
 }
 variable "vpc_id" {}
 
-variable "alb_sg_ids" {
-  type = list(string)
-}
-
-variable "controlplane_sg_ids" {
-  type = list(string)
-}
-
-variable "endpoint_sg_ids" {
-  type = list(string)
-}
-
-variable "app_port_min" { default = 30000 }
-variable "app_port_max" { default = 32767 }
-
 variable "tags" {
   type    = map(string)
   default = {}
