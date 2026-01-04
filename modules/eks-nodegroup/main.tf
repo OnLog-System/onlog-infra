473db3774c6d19b8908cd032252d9c01e0e60292
yoonseok.Kang <kangcl1609@gmail.com>
2026-01-04 19:39:53 +0900

    feat: fix EKS Node Group launch template reference and add count condition

--- modules/eks-nodegroup/main.tf
+++ modules/eks-nodegroup/main.tf
@@ -1,4 +1,6 @@
 resource "aws_eks_node_group" "this" {
+  count = var.enable ? 1 : 0
+
   cluster_name    = var.cluster_name
   node_group_name = "${var.environment}-${var.name}"
   node_role_arn  = var.node_role_arn
@@ -11,20 +13,16 @@ resource "aws_eks_node_group" "this" {
   }
 
   launch_template {
-    id      = aws_launch_template.this.id
+    id      = aws_launch_template.this[0].id
     version = "$Latest"
   }
 
   capacity_type = var.capacity_type
-  ami_type      = "AL2_ARM_64" # t4g 기준 (x86이면 AL2_x86_64)
+  ami_type      = "AL2_ARM_64"
 
   update_config {
     max_unavailable = 1
   }
 
   tags = var.tags
-
-  depends_on = [
-    aws_launch_template.this
-  ]
 }
