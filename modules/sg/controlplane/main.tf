0137dba4d3f8fbf0ea453f7728ab2738f8c45782
yoonseok.Kang <kangcl1609@gmail.com>
2026-01-04 18:30:11 +0900

    refactor: remove kubectl API access rules and admin CIDRs variable from control plane security group

--- modules/sg/controlplane/main.tf
+++ modules/sg/controlplane/main.tf
@@ -3,14 +3,5 @@ resource "aws_security_group" "controlplane" {
   description = "EKS Control Plane SG"
   vpc_id      = var.vpc_id
 
-  # kubectl → API Server
-  ingress {
-    description = "kubectl API access"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.admin_cidrs
-  }
-
   tags = var.tags
 }
