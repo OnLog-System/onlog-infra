19e4054d8ce1e22ab80c96e9e8189b1083a50981
yoonseok.Kang <kangcl1609@google.com>
2026-01-06 16:07:10 +0900

    feat: remove unnecessary egress rule from MSK security group

--- modules/sg/msk/main.tf
+++ modules/sg/msk/main.tf
@@ -3,12 +3,5 @@ resource "aws_security_group" "msk" {
   description = "MSK Broker SG"
   vpc_id      = var.vpc_id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = var.tags
 }
