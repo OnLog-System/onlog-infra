22250fab5799035aab3c4015f2e5c6c42ed5b90f
yoonseok.Kang <kangcl1609@google.com>
2026-01-07 22:24:18 +0900

    feat: remove unused EKS AMI data source and launch template references

--- env/dev/data.tf
+++
@@ -1,3 +0,0 @@
-data "aws_ssm_parameter" "eks_ami" {
-  name = "/aws/service/eks/optimized-ami/1.34/amazon-linux-2023/arm64/standard/recommended/image_id"
-}
--- modules/eks-nodegroup/launch_template.tf
+++ modules/eks-nodegroup/launch_template.tf
@@ -1,33 +1,33 @@
-resource "aws_launch_template" "this" {
-  count = var.enable ? 1 : 0
+# resource "aws_launch_template" "this" {
+#   count = var.enable ? 1 : 0
 
-  name_prefix = "${var.environment}-${var.name}-lt-"
-  update_default_version = true
+#   name_prefix = "${var.environment}-${var.name}-lt-"
+#   update_default_version = true
 
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+#   image_id      = var.ami_id
+#   instance_type = var.instance_type
+#   key_name      = var.key_name
 
-  vpc_security_group_ids = var.node_sg_ids
+#   vpc_security_group_ids = var.node_sg_ids
 
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      volume_size           = var.root_volume_size
-      volume_type           = "gp3"
-      iops                  = 3000
-      throughput            = 125
-      delete_on_termination = true
-    }
-  }
+#   block_device_mappings {
+#     device_name = "/dev/xvda"
+#     ebs {
+#       volume_size           = var.root_volume_size
+#       volume_type           = "gp3"
+#       iops                  = 3000
+#       throughput            = 125
+#       delete_on_termination = true
+#     }
+#   }
 
-  tag_specifications {
-    resource_type = "instance"
-    tags = merge(
-      var.tags,
-      { Name = "${var.environment}-${var.name}-node" }
-    )
-  }
+#   tag_specifications {
+#     resource_type = "instance"
+#     tags = merge(
+#       var.tags,
+#       { Name = "${var.environment}-${var.name}-node" }
+#     )
+#   }
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
