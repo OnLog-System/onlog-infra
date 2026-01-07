ab0cbd7ab1d46c528ee905f64b3c7ba1e29633d4
yoonseok.Kang <kangcl1609@google.com>
2026-01-07 15:12:01 +0900

    feat: disable MSK and EKS by default in variables configuration

--- env/dev/eks-access.tf
+++ env/dev/eks-access.tf
@@ -3,14 +3,16 @@
 ############################################################
 
 resource "aws_eks_access_entry" "admin" {
+  count         = var.enable_eks ? 1 : 0
   cluster_name  = module.eks_control_plane.cluster_name
   principal_arn = "arn:aws:iam::858923558183:user/terraform-dev-user"
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "admin" {
+  count         = var.enable_eks ? 1 : 0
   cluster_name  = module.eks_control_plane.cluster_name
-  principal_arn = aws_eks_access_entry.admin.principal_arn
+  principal_arn = aws_eks_access_entry.admin[0].principal_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
@@ -19,13 +21,15 @@ resource "aws_eks_access_policy_association" "admin" {
 }
 
 resource "aws_eks_access_entry" "yoonseok" {
+  count         = var.enable_eks ? 1 : 0
   cluster_name  = module.eks_control_plane.cluster_name
   principal_arn = "arn:aws:iam::858923558183:user/yoonseok.kang"
 }
 
 resource "aws_eks_access_policy_association" "yoonseok_admin" {
+  count         = var.enable_eks ? 1 : 0
   cluster_name  = module.eks_control_plane.cluster_name
-  principal_arn = aws_eks_access_entry.yoonseok.principal_arn
+  principal_arn = aws_eks_access_entry.yoonseok[0].principal_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
