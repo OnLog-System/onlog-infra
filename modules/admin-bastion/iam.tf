54a4adb80e0d5a6ed4161f73b12b83836854a600
yoonseok.Kang <kangcl1609@google.com>
2026-01-07 23:08:32 +0900

    feat: update EKS access policy to allow DescribeCluster for Admin Bastion

--- modules/admin-bastion/iam.tf
+++ modules/admin-bastion/iam.tf
@@ -27,10 +27,27 @@ resource "aws_iam_instance_profile" "this" {
 }
 
 ############################################
-# EKS ReadOnly Access for Admin Bastion
+# EKS DescribeCluster access for Admin Bastion
 ############################################
 
-resource "aws_iam_role_policy_attachment" "eks_readonly" {
+resource "aws_iam_policy" "eks_describe_cluster" {
+  name = "${var.environment}-eks-describe-cluster"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster"
+        ]
+        Resource = "arn:aws:eks:ap-northeast-2:${data.aws_caller_identity.current.account_id}:cluster/dev-eks"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_describe_cluster" {
   role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSReadOnlyAccess"
+  policy_arn = aws_iam_policy.eks_describe_cluster.arn
 }
