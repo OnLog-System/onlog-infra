9f328795648c04bc8731d4c8a14e1b9886320438
yoonseok.Kang <kangcl1609@google.com>
2026-01-10 17:03:05 +0900

    feat: add region variable to MSK module configuration

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -162,6 +162,7 @@ module "msk" {
   source                   = "../../modules/msk"
   name                     = "msk"
   environment              = var.environment
+  region                   = var.region
   kafka_version            = "3.8.x"
   availability_zones       = var.availability_zones
   brokers_per_az           = 1
