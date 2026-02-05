e5b01d8279a53a4023aa04a22351f914ab105748
yoonseok.Kang <kangcl1609@google.com>
2026-02-05 14:08:44 +0900

    feat: refactor main.tf to use local tags for VPC and security group modules

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,27 +1,33 @@
 ############################################################
-# 1. VPC (Single AZ / Minimal)
+# Common Tags
+############################################################
+locals {
+  tags = {
+    Project     = "onlog"
+    Environment = "prod"
+    ManagedBy   = "terraform"
+  }
+}
+
+############################################################
+# 1. VPC (Single AZ / Single Public Subnet)
 ############################################################
 module "vpc" {
   source = "../../modules/vpc"
 
   vpc_cidr           = "10.20.0.0/16"
   availability_zones = ["ap-northeast-2a"]
+
   public_subnet_cidrs  = ["10.20.1.0/24"]
   private_subnet_cidrs = []
 
   enable_nat_gateway = false
   environment        = "prod"
-
-  tags = {
-    Project     = "onlog"
-    Environment = "prod"
-  }
+  tags               = local.tags
 }
 
 ############################################################
 # 2. Security Group (onlog prod)
-# - SSH (restricted)
-# - 80 / 443 public
 ############################################################
 module "sg_onlog_prod" {
   source = "../../modules/sg/onlog-prod"
@@ -30,14 +36,17 @@ module "sg_onlog_prod" {
   vpc_id      = module.vpc.vpc_id
   environment = "prod"
 
-  tags = {
-    Project     = "onlog"
-    Environment = "prod"
-  }
+  ssh_allowed_cidrs = var.ssh_allowed_cidrs
+
+  tags = local.tags
 }
 
 ############################################################
-# 3. EC2 (FastAPI + TimescaleDB + Grafana)
+# 3. EC2 (All-in-One)
+# - FastAPI
+# - TimescaleDB
+# - Grafana
+# - Batch / Replay
 ############################################################
 module "onlog_prod_ec2" {
   source = "../../modules/timescaledb"
@@ -48,16 +57,17 @@ module "onlog_prod_ec2" {
   subnet_id          = module.vpc.public_subnet_ids[0]
   security_group_ids = [module.sg_onlog_prod.id]
 
-  instance_type    = "t4g.large"
-  key_name         = "onlog-prod-key"
+  instance_type = "t4g.large"
+  key_name      = var.key_name
 
   # Storage
   data_volume_size = 100
   wal_volume_size  = 50
 
-  tags = {
-    Project     = "onlog"
-    Environment = "prod"
-    Role        = "all-in-one"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Role = "all-in-one"
+    }
+  )
 }
